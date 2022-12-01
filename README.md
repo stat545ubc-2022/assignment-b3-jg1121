2699d145cfbac3d26ab857e54e5152691377d11f
jg1121 <jg1121@student.ubc.ca>
2022-12-01 10:44:48 -0800

    Assignment 4: Added new features, updated readme, and republished

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 Welcome to my first exploration of a shiny app! [View my shiny app here](https://jg1121.shinyapps.io/assignment-b3-jg1121/)
 
-This repository is for assignment 3 of STAT545B at UBC. 
+This repository is for assignments 3 and 4 of STAT545B at UBC. 
+
+## Asignment 3
 
 For this assignment I chose to do Option A of adding features to our BC Liquor app.
 
@@ -14,12 +16,27 @@ I added three new features to the "basic" BC Liquor app that we designed in clas
 
 - I used the DT package to turn the static table into an interactive table with a search bar and sorting options. This helps the user look for exactly what they want. For example, if they wanted to sort wine by the cheapest cost, they would now be able to. 
 
+## Assignment 4
+For this assignment, I built off of the last version of the website from assignment 3 to improve the quality and overall design of the app.
 
-## Repository Contents
-- www folder contains the link to the image I used
+- I added the cosmo theme from shiny themes to the UI to enhance the appearance of the page 
+
+- I added a title panel to the UI
+
+- I added a description of the website for users to read with borders and an image of clinking glasses.
+
+- I converted the radiobuttons feature into checkboxes for users to choose multiple options
+
+- I added the number of results to be shown when the user filters for their selection
+
+- I added a scatterplot (in a new tab) of alcohol content vs price, colored by alcohol subtype, for users to visualize their options within their selection.
+
+
+### Repository Contents
+- www folder contains the link to the images I used
 - app.R contains the code for the shiny app
 
 
 
-### Data
+#### Data
 The BC Liquor data set was pulled from: https://github.com/daattali/shiny-server/blob/master/bcl/data/bcl-data.csv
--- app.R
+++ app.R
@@ -1,23 +1,40 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(shinythemes)
 
 bcl <- read.csv("bcl-data.csv", stringsAsFactors = FALSE)
 
 ui <- fluidPage(
-  img(src = "logo.png", height="50%", width="50%"), #I added an image to the UI of the BC Liquor Store logo as it makes the webpage more visually appealing. I also reduced the size of this image to take up an appropriate amount of the webpage.
-  sidebarLayout(
+  #Adding the cosmo theme from shinythemes to enhance the UI
+  theme = shinytheme("cosmo"),
+  #I added an image to the UI of the BC Liquor Store logo as it makes the webpage more visually appealing. I also reduced the size of this image to take up an appropriate amount of the webpage.
+  img(src = "logo.png", height="50%", width="50%"),
+  #Adding a title
+  titlePanel("All about BC Liquor Store products!"),
+  #Adding a border
+  tags$hr(style="border-color: red;"),
+  #Adding a header for info about the app with an image of clinking glasses
+  h5("On this shinyapp you can filter the BC Liquor Store data to find the products you want based on liquor type, alcohol content, price, and more. You can visualize your options using the histogram and scatter plot tabs as well. Have fun exploring!", img(src = "liquor.png", height="5%", width="5%")),
+  tags$hr(style="border-color: red;"),
+  #Laying out page with panels
+   sidebarLayout(
     sidebarPanel(
       sliderInput("priceInput", "Price", 0, 100, c(25, 40), pre = "$"),
-      radioButtons("typeInput", "Product type",
+      #I changed the radiobuttons into check boxes to select multiple features at once
+      checkboxGroupInput("typeInput", "Product type",
                    choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
                    selected = "WINE"),
-      uiOutput("countryOutput")
+      uiOutput("countryOutput"),
+    #Displaying number of results
+     textOutput("NumberofResults")
     ),
     mainPanel(
-      tabsetPanel( #I added two tabs: one with the plot, one with the table. This helps organize the webpage.
+      ##I added three tabs: one with the table, one with a histogram, one with a scatterplot. This helps organize the webpage.
+      tabsetPanel(
         tabPanel("Table", DT::dataTableOutput("results")), #I replaced TableOutput() with DT:dataTableOutput() to include the new interactive table I made in the server
-        tabPanel("Plot", plotOutput("coolplot"))
+        tabPanel("Histogram", plotOutput("histogram")),
+        tabPanel("Scatter Plot", plotOutput("scatterplot"))
       )
     )
   )
@@ -43,7 +60,7 @@ server <- function(input, output) {
       )
   })
 
-  output$coolplot <- renderPlot({
+  output$histogram <- renderPlot({
     if (is.null(filtered())) {
       return()
     }
@@ -54,6 +71,13 @@ server <- function(input, output) {
   output$results <- DT::renderDataTable({ #I replaced the renderTable() with DT:renderDataTable() to turn our static table into an interactive one. This added a search bar and sorting capabilities.
     filtered()
   })
+    output$scatterplot <- renderPlot({filtered() %>%
+    ggplot(aes(Alcohol_Content, Price)) + geom_point(size=3, aes(color= Subtype)) + scale_colour_discrete("Subtype")
+  })
+    output$NumberofResults <- renderText({
+      paste0("Results: ", nrow(filtered())) #I added the number of results to be shown
+
+    })
 }
 
 shinyApp(ui = ui, server = server)
